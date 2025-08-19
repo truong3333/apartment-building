32e0201f278778325b46542d3d1b2adf9f48488e
truong3333 <truong0962935943@gmail.com>
2025-08-19 22:19:51 +0700

    cập nhật lại dự án

--- src/main/java/com/example/apartment/configuration/SwaggerConfig.java
+++ src/main/java/com/example/apartment/configuration/SwaggerConfig.java
@@ -2,7 +2,12 @@ package com.example.apartment.configuration;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @SecurityScheme(
@@ -12,4 +17,9 @@ import org.springframework.context.annotation.Configuration;
         bearerFormat = "JWT"
 )
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(new Server().url("https://apartment-building-production.up.railway.app")));
+    }
 }
